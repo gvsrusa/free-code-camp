2bdb9a8ec79a6f9d6ac82eb93e68ed7d06184780
gvsrusa <gvsrusa@gmail.com>
2018-08-24 08:53:44 -0600

    new files added

--- Javascript-Algorithms-And-Data-Structures-Certification-300hours/Intermediate-Algorithm-Scripting/wherefore-art-thou.js
+++ Javascript-Algorithms-And-Data-Structures-Certification-300hours/Intermediate-Algorithm-Scripting/wherefore-art-thou.js
@@ -1,17 +1,17 @@
 function whatIsInAName(collection, source) {
 
-    var srcKeys = Object.keys(source);
-  
-    // filter the collection
-    return collection.filter(function (obj) {
-      return srcKeys
-        .map(function(key) {
-          return obj.hasOwnProperty(key) && obj[key] === source[key];
-        })
-        .reduce(function(a, b) {
-          return a && b;
-        });
-    });
-  }
-  
-  whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 })
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(obj=>{
+    return srcKeys
+      .map(key=>{
+        return obj.hasOwnProperty(key) && obj[key] === source[key];
+      })
+      .reduce((a, b)=> {
+        return a && b;
+      });
+  });
+}
+
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 })
